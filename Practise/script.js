cca41ad02331979cb991fcc1da4325ada05cbea0
Eren-Yeag <esenbaevnasyr@gmail.com>
2023-04-10 06:00:30 +0000

    first lesson with code

--- Practise/script.js
+++ Practise/script.js
@@ -1,2 +1,30 @@
-const quote = 'THE COMEBACK';
-console.log(quote)
+"use strict";
+// 1. Переменные и строгий режим
+//
+
+let number = 5;
+const day = 1;
+
+number = 10;
+console.log(number);
+
+const object = {
+  a: 18
+};
+
+object.a = 20;
+console.log(object.a);
+
+console.log(pau); // Виден до объявления перемнной
+var pau = 15;
+
+{
+    var res = 35; //ЗАМЫКАНИЕ
+}
+console.log(res);
+
+// a = 59;
+// console.log(a);
+
+//
+// 1
