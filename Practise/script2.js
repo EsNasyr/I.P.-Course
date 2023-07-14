acbc28636842e0fd4e46db4541f52f1d9304537e
Nasyr Esenbaev <esenbaevnasyr@gmail.com>
2023-07-14 20:06:17 +0000

    tasks 1

--- Practise/script2.js
+++ Practise/script2.js
@@ -238,7 +238,7 @@
 // ---------- 8
 // 9.Циклы ----------
 //
-let num = 50;
+// let num = 50;
 // while (num < 55) {
 //     console.log(num);
 //     num++
@@ -282,16 +282,16 @@ let num = 50;
 // }
 // console.log(result)
 
-first: for(let i = 0; i < 3; i++) {
-    console.log(`First level: ${i}`);
-    for(let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`);
-        for(let k = 0; k < 3; k++) {
-            if (k === 2) continue first;
-            console.log(`Third level: ${k}`);
-        }
-    }
-}
+// first: for(let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for(let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for(let k = 0; k < 3; k++) {
+//             if (k === 2) continue first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
 //
 // ---------- 10
 
--- Tasks/script3.js
+++ Tasks/script3.js
@@ -1,3 +1,5 @@
+// 8.Логические операторы ----------
+//
 // console.log( NaN || 2 || undefined ); //2
 //
 // console.log( NaN && 2 && undefined ); //NaN
@@ -43,6 +45,40 @@
 // if (hamburger && cola || fries === 3 && nuggets) {
 //     console.log('Done!')
 // } //нет
+//
+// ---------- 8
+// Tasks 1
+//
+// for (let i = 5; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 20; i > 10; i--) {
+//     if (i === 13) break;
+//     console.log(i)
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 1) continue;
+//     console.log(i)
+// }
+// let i = 2;
+// while(i <= 16) {
+//     if (i % 2 === 0) {
+//         i++;
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// const arrayOfNumbers = [];
+// for (let i = 5; i <= 10; i++) {
+//     arrayOfNumbers[i - 5] = i;
+// }
+// console.log(arrayOfNumbers);
+// Tasks 1
 
 
 
