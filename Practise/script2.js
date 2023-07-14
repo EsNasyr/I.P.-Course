d754a55379d85b1c130591134fb265e31f44ff06
Nasyr Esenbaev <esenbaevnasyr@gmail.com>
2023-07-14 16:55:41 +0000

    9 - lesson

--- Practise/script2.js
+++ Practise/script2.js
@@ -236,3 +236,31 @@
 // console.log(!0);
 //
 // ---------- 8
+// 9.Циклы ----------
+//
+let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//     num++
+// }
+
+// do {
+//     console.log(num);
+//     num++
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         //break;
+//         continue
+//     }
+//     console.log(i);
+// }
+//
+// ---------- 9
+
