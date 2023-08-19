ffe2466633a14beded5e293e5c309d2e9a04433e
Nasyr Esenbaev <esenbaevnasyr@gmail.com>
2023-08-19 17:54:40 +0000

    15 - lesson

--- Practise/script2.js
+++ Practise/script2.js
@@ -411,9 +411,29 @@
 // ---------- 14
 
 // 15. Callback - функции ----------
+// Callback - функция которая должна быть выполнена, после того как другая фунция завершила выполенение
 function first() {
     // Do something
-    
-
+    setTimeout(function () {
+        console.log(1)
+    }, 500);
 }
+
+function second() {
+    console.log(2)
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу ${lang}`);
+    callback();
+}
+
+function done () {
+    console.log('Я прошел этот урок')
+}
+
+learnJS('JavaScript', done);
 // ---------- 15
